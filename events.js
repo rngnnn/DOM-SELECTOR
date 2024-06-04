2961d6c4ffefc7e20ea2c6500d27764f91c5fc22
rngnnn <rngnnnclk@gmail.com>
2024-06-04 11:06:27 +0200

    events

---
+++ events.js
@@ -0,0 +1,23 @@
+//? METHOD-1 (SADECE JAVASCRİPT TE YAZILAN YOL) 1.yol
+//?---------------------------------------------------------------
+
+
+//*example1 onmouseover, onmouseout
+
+const js= document.querySelector("#js")
+const badi= document.querySelector("#badi")
+
+js.onmouseover=()=>{
+
+badi.style.background="pink"
+
+
+
+}
+js.onmouseout=()=>{
+
+    badi.style.background="orange"
+
+}
+
+//*exapmle2 onclick ondoubleclick
