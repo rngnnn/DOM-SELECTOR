088e250d307ff774828e7846ab338fa8962a29aa
rngnnn <rngnnnclk@gmail.com>
2024-06-04 09:41:31 +0200

    DOM-Selector

---
+++ selector.js
@@ -0,0 +1,33 @@
+
+
+//* ======================================================
+//*                   GETELEMENTBYID()
+//* ======================================================
+
+//*example1 buton style
+
+
+
+
+const buton=document.getElementById("btn")
+
+buton.style.width="200px"
+buton.style.height="80px"
+buton.style.fontSize="80px"
+buton.style.backgroundColor="red"
+
+
+
+const paragraf=document.getElementById("par").style
+
+paragraf.width="300px"
+paragraf.color="red"
+paragraf.background="yellow"
+paragraf.margin="220px"
+
+
+
+//* ======================================================
+//*                   GETELEMENTSBYTAGNAME()
+//* tag ismiyle çağırıyoruz.aynı tag den çok olacağı için index le hangisini seçtiğimizi belirtiyoruz (HTMLCollection- Array özellikleri gösterir)
+//*================================================
